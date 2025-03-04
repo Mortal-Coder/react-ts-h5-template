1c3c71652266f6cd82e854ea1929fef4923e0ccd
Mortal-Coder <johnlizhao@gmail.com>
2025-03-04 11:07:12 +0800

    feat(about): optimize language switching

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,16 +1,44 @@
+import { LangEnum } from '@/constant/langEnum';
 import { useLanguageStore } from '@/store';
-import { Button } from 'antd-mobile';
+import { Button, Modal } from 'antd-mobile';
 import { FC } from 'react';
-
+import { useTranslation } from 'react-i18next';
 const About: FC = () => {
   const { setLanguage } = useLanguageStore();
+  const { t } = useTranslation();
+
+  const handleChangeLanguage = () => {
+    Modal.show({
+      title: t('common.changeLanguage'),
+      closeOnAction: true,
+      closeOnMaskClick: true,
+      onAction: (action) => {
+        setLanguage(action.key as string);
+      },
+      actions: [
+        {
+          key: LangEnum.ZH,
+          text: '中文',
+          style: {
+            color: 'var(--color-primary)',
+            fontSize: 16,
+          },
+        },
+        {
+          key: LangEnum.EN,
+          text: 'English',
+          style: {
+            color: 'var(--color-primary)',
+            fontSize: 16,
+          },
+        },
+      ],
+    });
+  };
   return (
     <div className="flex flex-col items-center justify-center gap-3">
-      <Button onClick={() => setLanguage('zh')} className="w-12">
-        中文
-      </Button>
-      <Button onClick={() => setLanguage('en')} className="w-12">
-        English
+      <Button onClick={handleChangeLanguage}>
+        {t('common.changeLanguage')}
       </Button>
     </div>
   );
